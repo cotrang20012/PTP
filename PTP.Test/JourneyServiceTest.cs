905d08c9bc52e4227a326f739610992be15bb453
cotrang20012 <cotrang2012@gmail.com>
2023-07-08 22:08:53 +0700

    Update unit test

---
+++ PTP.Test/JourneyServiceTest.cs
@@ -0,0 +1,109 @@
+﻿using PTP.Core.Domain.Entities;
+using PTP.Core.Domain.Enums;
+using PTP.Core.Dtos;
+using PTP.Test.Services;
+using PTP.Validator;
+
+namespace PTP.Test
+{
+    [TestFixture]
+    public class JourneyServiceTest : JourneyTestService
+    {
+        [Test]
+        public void InsertNewJourneyValidatorTest_ValidRequest()
+        {
+            UpsertJourneyRequestDto upsertJourneyRequest = new UpsertJourneyRequestDto() { Name = "Company Trip", Description = "A trip with company at ...", CountryId = 2, CountryName = "Viet Nam", PlaceId = "3", PlaceName = "Đà Nẵng", CurrencyId = 3, CurrencyName = "VND", Amount = 5000000, Status = JourneyStatus.Planning.ToString(), StartDate = DateTime.Now.Date, EndDate = DateTime.Now.AddDays(5).Date, Days = 5, Nights = 4 };
+            upsertJourneyRequest.EndDate = upsertJourneyRequest.EndDate.Date;
+            upsertJourneyRequest.StartDate = upsertJourneyRequest.StartDate.Date;
+            var insertValidator = new InsertNewJourneyValidator();
+
+            var insertValidationResult = insertValidator.Validate(upsertJourneyRequest);
+
+            Assert.IsTrue(insertValidationResult.IsValid);
+        }
+        public void InsertNewJourneyValidatorTest_UnvalidRequest()
+        {
+            UpsertJourneyRequestDto upsertJourneyRequest = new UpsertJourneyRequestDto() { Description = "Short trip", CountryId = 2, PlaceId = "3", Amount = 5000000, Status = JourneyStatus.Planning.ToString(), StartDate = DateTime.Now.Date.AddDays(5).Date, EndDate = DateTime.Now, Days = 5, Nights = 4 };
+            upsertJourneyRequest.EndDate = upsertJourneyRequest.EndDate.Date;
+            upsertJourneyRequest.StartDate = upsertJourneyRequest.StartDate.Date;
+            var insertValidator = new InsertNewJourneyValidator();
+
+            var insertValidationResult = insertValidator.Validate(upsertJourneyRequest);
+
+            Assert.IsFalse(insertValidationResult.IsValid);
+        }
+        [Test]
+        public async Task InsertNewJourneyTest_Success()
+        {
+            UpsertJourneyRequestDto upsertJourneyRequest = new UpsertJourneyRequestDto() { Name = "Company Trip", Description = "A trip with company at ...", CountryId = 2, PlaceId = "3", CurrencyId = 3, Amount = 5000000, Status = JourneyStatus.Planning.ToString(), StartDate = DateTime.Now.Date, EndDate = DateTime.Now.AddDays(5).Date, Days = 5, Nights = 4 };
+            upsertJourneyRequest.EndDate = upsertJourneyRequest.EndDate.Date;
+            upsertJourneyRequest.StartDate = upsertJourneyRequest.StartDate.Date;
+            var entity = _mapper.Map<Journey>(upsertJourneyRequest);
+
+            await _journeyRepository.AddAsync(entity);
+            await _journeyRepository.SaveChangesAsync();
+            _context.DbContext.ChangeTracker.Clear();
+            var listJourneys = await _journeyRepository.GetAllAsyncNoTracking();
+
+            Assert.IsNotNull(listJourneys);
+            Assert.AreEqual(7, listJourneys.Count());
+        }
+
+        [Test]
+        public async Task DeleteJourneyTest_JourneyExist()
+        {
+            var journeyId = 1;
+            var journeyEntity = await _journeyRepository.GetAsync(journeyId);
+            _journeyRepository.Delete(journeyEntity);
+            await _journeyRepository.SaveChangesAsync();
+            _context.DbContext.ChangeTracker.Clear();
+            var listJourneys = await _journeyRepository.GetAllAsyncNoTracking();
+
+            Assert.IsNotNull(listJourneys);
+            Assert.AreEqual(5, listJourneys.Count());
+        }
+        [Test]
+        public async Task DeleteJourneyTest_JourneyNotExist()
+        {
+            var journeyId = 8;
+            var journeyEntity = await _journeyRepository.GetAsync(journeyId);
+            
+            Assert.AreEqual(default(Journey), journeyEntity);  
+        }
+        [Test]
+        public void UpdateJourneyValidator_ValidRequest()
+        {
+            Assert.Pass();
+        }
+        [Test]
+        public void UpdateJourneyValidator_UnvalidRequest()
+        {
+            Assert.Pass();
+        }
+        [Test]
+        public async Task UpdateJourney_JourneyExist()
+        {
+            Assert.Pass();
+        }
+        [Test]
+        public async Task UpdateJourney_JourneyNotExist()
+        {
+            Assert.Pass();
+        }
+        [Test]
+        public async Task RemovePlacesFromJourneyTest()
+        {
+            Assert.Pass();
+        }
+        [Test]
+        public async Task IsJourneyExistTest_J()
+        {
+            Assert.Pass(); 
+        }
+        [Test]
+        public async Task IsCurrencyExist()
+        {
+            Assert.Pass();
+        }
+    }
+}
---
+++ PTP.Test/Services/TestService.cs
@@ -0,0 +1,51 @@
+﻿using AutoMapper;
+using NSubstitute;
+using NUnit.Framework;
+using PTP.Core.Domain.Entities;
+using PTP.Core.Interfaces.Repositories;
+using PTP.Core.Interfaces.Services;
+using PTP.MappingProfiles;
+using PTP.Repositories;
+using PTP.Test.Data;
+using System.Reflection;
+
+
+namespace PTP.Test.Services
+{
+    [SetUpFixture]
+    public class JourneyTestService
+    {
+        protected InMemoryPTPControllerTestContext _context;
+        protected IRepository<Journey> _journeyRepository;
+        protected IRepository<Country> _countryRepository;
+        protected IRepository<Currency> _currencyRepository;
+        protected IRepository<Place> _placeRepository;
+        protected IMapper _mapper;
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
+        {
+            _context = new InMemoryPTPControllerTestContext();
+            _journeyRepository = new Repository<Journey>(_context.GetDatabaseContext());
+            _countryRepository = new Repository<Country>(_context.GetDatabaseContext());
+            _currencyRepository = new Repository<Currency>(_context.GetDatabaseContext());
+            _placeRepository = new Repository<Place>(_context.GetDatabaseContext());
+            var configuration = new MapperConfiguration(cfg => cfg.AddProfile(new ProjectAutoMapperProfile()));
+            _mapper = configuration.CreateMapper();
+        }
+        [SetUp]
+        public void Setup()
+        {
+            _context.CreateDataForDatabase();
+        }
+        [TearDown]
+        public void TearDown()
+        {
+            _context.CleanUp();
+        }
+        [OneTimeTearDown]
+        public void OneTimeTearDown()
+        {
+            _context.CleanUpDatabaseAndDispose();
+        }
+    }
+}
